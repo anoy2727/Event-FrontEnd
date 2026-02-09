308bb02320a0aaf62984913734439aa62b176ca5
Anoy <raiyananoy@gmail.com>
2026-02-09 21:49:58 +0600

    Update API URL to deployed backend

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ANOY</div>
+          <div className="right">
+            <p>Gulshan, Dhaka</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CODEWITHANOY</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithanoy.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
